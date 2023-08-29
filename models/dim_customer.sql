53fc8d7ac0e62d277fb54d8b8bcc17bb8648e6b3
ruchitdesai22@gmail.com <ruchitdesai22@gmail.com>
2023-08-29 12:33:21 +0000

    change db

--- models/dim_customer.sql
+++ models/dim_customer.sql
@@ -5,7 +5,7 @@ with customers as (
         first_name,
         last_name
 
-    from raw.jaffle_shop.customers
+    from default.jaffle_shop_customers
 
 ),
 
@@ -17,7 +17,7 @@ orders as (
         order_date,
         status
 
-    from raw.jaffle_shop.orders
+    from default.jaffle_shop_orders
 
 ),
 
